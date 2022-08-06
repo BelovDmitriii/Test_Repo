6328949d1ffb31b883f8e02d212d8c71016946dd
Dmitrii Belov <belovd90@mail.ru>
2022-08-07 02:15:53 +0300

    добавлены компоненты и небольшой функционал

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,25 @@
 import React, { useState } from 'react';
 import Product from './components/product/product';
+import { products } from './components/data/products';
 
 function App() {
   const[count, setCount] = useState(0);
   return (
     <>
-    <div className='container mx-auto max-w-2xl pt-5'>
-      <h1>Hello Test Page</h1>
-      <h2 className='font-bold'>{`Test JSX ${count}`}</h2>
-      <button
-        className='py-2 px-4 border'
-        onClick={() => {
-          setCount(count + 1);
-        }}
-      >
-        Нажми меня
-      </button>
-    </div>
-    <Product />
+      <div className='container mx-auto max-w-2xl pt-5'>
+        <h1>Hello Test Page</h1>
+        <h2 className='font-bold'>{`Test JSX ${count}`}</h2>
+        <button
+          className='py-2 px-4 border'
+          onClick={() => {
+            setCount(count + 1);
+          }}
+        >
+          Нажми меня
+        </button>
+        <Product product={products[0]}/>
+        <Product product={products[1]}/>
+      </div>
     </>
   );
 }
--- src/components/product/product.tsx
+++ src/components/product/product.tsx
@@ -1,6 +1,27 @@
-function Product(){
+import { useState } from 'react';
+import { IProduct } from '../../types/types';
+
+interface ProductProps {
+    product: IProduct;
+}
+
+function Product({ product }: ProductProps){
+    const [details, setDetails] = useState(false);
     return (
-        <div>Product</div>
+        <div className='border py-2 px-4 rounded flex  flex-col items-center mb-2'>
+            <img src={product.image} className='w-1/6' alt={product.title} />
+            <p>{product.title}</p>
+            <p className='font-bold'>{product.price}</p>
+            <button 
+                className='py-2 px-4 border bg-green-400'
+                onClick={() => setDetails(prev => !prev)}
+            >
+                Show Details
+            </button>
+            {details && <div>
+                <p>{product.description}</p>
+            </div>}
+        </div>
     );
 }
 
