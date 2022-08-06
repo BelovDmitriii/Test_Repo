24e153513321e118f8bc20678d089c65c8c1cdf6
Dmitrii Belov <belovd90@mail.ru>
2022-08-07 01:05:08 +0300

    настройка рабочей среды

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,24 @@
 import React, { useState } from 'react';
+import Product from './components/product/product';
 
 function App() {
   const[count, setCount] = useState(0);
   return (
-    <div>
+    <>
+    <div className='container mx-auto max-w-2xl pt-5'>
       <h1>Hello Test Page</h1>
+      <h2 className='font-bold'>{`Test JSX ${count}`}</h2>
       <button
         className='py-2 px-4 border'
         onClick={() => {
-          setCount(count + 1)
-          console.log(count);
+          setCount(count + 1);
         }}
       >
         Нажми меня
       </button>
     </div>
+    <Product />
+    </>
   );
 }
 
