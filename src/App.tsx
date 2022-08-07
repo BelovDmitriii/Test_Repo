ffeceb01f7c99d8e72f4581a17750e2dfe171d34
Dmitrii Belov <belovd90@mail.ru>
2022-08-08 01:28:34 +0300

    добавлена логика работы с сервером

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,35 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Product from './components/product/product';
-import { products } from './components/data/products';
+import axios, { AxiosError } from 'axios';
+import { IProduct } from './types/types';
 
 function App() {
-  const[count, setCount] = useState(0);
+  const[products, setProducts] = useState<IProduct[]>([]);
+  const[loading, setLoading] = useState(false);
+  const[error, setError] = useState('');
+
+  async function fetchProducts () {
+    try {
+      setError('');
+      setLoading(true);
+      const response = await axios.get<IProduct[]>('https://fakestoreapi.com/products');
+      setProducts(response.data);
+      setLoading(false);
+    } catch (e: unknown) {
+      const error = e as AxiosError
+      setLoading(false);
+      setError(error.message);
+    }
+  }
+ 
+  useEffect(() => {
+    fetchProducts()
+  },[]);
   return (
     <>
       <div className='container mx-auto max-w-2xl pt-5'>
-        <h1>Hello Test Page</h1>
-        <h2 className='font-bold'>{`Test JSX ${count}`}</h2>
-        <button
-          className='py-2 px-4 border'
-          onClick={() => {
-            setCount(count + 1);
-          }}
-        >
-          Нажми меня
-        </button>
+        {loading && <p className='text-center'>Loading...</p>}
+        {error && <p className='text-center text-red-600'>{ error }</p>}
         {products.map(product => <Product product={product} key={product.id}/>)}
       </div>
     </>
