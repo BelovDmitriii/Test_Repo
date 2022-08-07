e349d576ea89363418538ff32097232754004582
Dmitrii Belov <belovd90@mail.ru>
2022-08-08 00:40:26 +0300

    реализована динамическая смена стилей кнопки

--- src/components/product/product.tsx
+++ src/components/product/product.tsx
@@ -7,16 +7,19 @@ interface ProductProps {
 
 function Product({ product }: ProductProps){
     const [details, setDetails] = useState(false);
+
+    const btnBgClassName = details ? 'bg-blue-400' : 'bg-green-400';
+    const btnBgClasses = ['py-2 px-4 border', btnBgClassName];
     return (
         <div className='border py-2 px-4 rounded flex  flex-col items-center mb-2'>
             <img src={product.image} className='w-1/6' alt={product.title} />
             <p>{product.title}</p>
             <p className='font-bold'>{product.price}</p>
             <button 
-                className='py-2 px-4 border bg-green-400'
+                className= {btnBgClasses.join(' ')}
                 onClick={() => setDetails(prev => !prev)}
             >
-                Show Details
+                {details ? 'Hide Detais' : 'Show Details'}
             </button>
             {details && <div>
                 <p>{product.description}</p>
