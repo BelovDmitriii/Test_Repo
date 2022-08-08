cc9d7b6098cedc7e52fff1731ad1dee1568d9dfb
Dmitrii Belov <belovd90@mail.ru>
2022-08-09 00:23:45 +0300

    добавлен функционал модального окна

--- src/components/create-product/createProduct.tsx
+++ src/components/create-product/createProduct.tsx
@@ -1,11 +1,37 @@
 import { useState } from 'react';
+import { IProduct } from '../../types/types';
+import ErrorMessage from '../errorMessage/errorMessage';
+import axios from 'axios';
+
+const productData: IProduct = {
+  title: 'test product',
+  price: 13.5,
+  description: 'lorem ipsum set',
+  image: 'https://i.pravatar.cc',
+  category: 'electronic',
+  rating: {
+    rate: 44,
+    count: 5546
+  }
+};
 
 function CreateProduct() {
 
   const [value, setValue] = useState('');
+  const [error, setError] = useState('');
 
-  const submitHandler = (evt: React.FormEvent) => {
+  const submitHandler = async (evt: React.FormEvent) => {
     evt.preventDefault();
+    setError('');
+
+    if(value.trim().length === 0) {
+      setError('Please enter valid value!');
+      return;
+    }
+
+    productData.title = value;
+
+    await axios.post<IProduct>('https://fakestoreapi.com/products', productData)
   }
 
   const changeHandler = (evt: React.KeyboardEvent<HTMLInputElement>) => {
@@ -22,6 +48,9 @@ function CreateProduct() {
         value={value}
         onChange = {changeHandler}
       />
+
+      {error && <ErrorMessage error={error}/>}
+
       <button
         type='submit'
         className='py-2 px-4 border bg-yellow-400 hover:text-white'
