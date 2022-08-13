4ecb616a946f26d994fe7068cf7579add3b2fb77
Dmitrii Belov <belovd90@mail.ru>
2022-08-14 01:34:38 +0300

    отображает реальное количество оставшихся допустимых для ввода символов

--- src/components/about/about.tsx
+++ src/components/about/about.tsx
@@ -1,11 +1,20 @@
-function About() {
+type AboutProps ={
+  maxLength: number;
+  onChange: (target:any) => void;
+  value: string;
+}
+
+function About({maxLength, onChange, value }: AboutProps): JSX.Element {
   return(
     <>
       <textarea
+        onChange={onChange}
+        value={value}
+        maxLength={maxLength}
         cols={60}
         rows={5}
       />
-      <p className="symbols-count">Осталось символов: 0</p>
+      <p className="symbols-count">Осталось символов: {maxLength - value.length} </p>
     </>
   );
 }
--- src/pages/about-page.tsx
+++ src/pages/about-page.tsx
@@ -2,15 +2,22 @@ import { useState } from "react";
 import About from "../components/about/about";
 import Counter from "../components/counter/counter";
 
+const DEFAULT_MAX_LENGTH = 50;
+const COUNTER_STEP = 5;
+
 export function AboutPage () {
-  const [count, setCount] = useState();
+  const [count, setCount] = useState(DEFAULT_MAX_LENGTH);
   const [text, setText] = useState('');
   return (
     <>
       <div className="w-[500px] p-5 m-auto bg-blue-100 top-10 align-center">
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus excepturi, dolore fugiat commodi eius quo repudiandae quidem nisi distinctio minus quibusdam qui praesentium aliquam labore ex quam perferendis dolorum dicta non blanditiis inventore! Sapiente a perspiciatis, perferendis, ab quaerat porro dolore laudantium labore iusto eaque quod exercitationem, autem ullam eveniet.
         <br/>
-        <About />
+        <About 
+          onChange={({target}) => setText(target.value)}
+          maxLength = {count}
+          value = {text}
+          />
         <Counter />
       </div>
     </>
