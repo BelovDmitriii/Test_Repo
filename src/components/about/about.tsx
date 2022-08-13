0014973b1400e3d212e16256fa3f8f26d85f6143
Dmitrii Belov <belovd90@mail.ru>
2022-08-14 01:02:35 +0300

    Добавлен компонент ввода символов

---
+++ src/components/about/about.tsx
@@ -0,0 +1,13 @@
+function About() {
+  return(
+    <>
+      <textarea
+        cols={60}
+        rows={5}
+      />
+      <p className="symbols-count">Осталось символов: 0</p>
+    </>
+  );
+}
+
+export default About;
--- src/pages/about-page.tsx
+++ src/pages/about-page.tsx
@@ -1,7 +1,13 @@
+import About from "../components/about/about";
+
 export function AboutPage () {
   return (
+    <>
       <div className="w-[500px] p-5 m-auto bg-blue-100 top-10 align-center">
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus excepturi, dolore fugiat commodi eius quo repudiandae quidem nisi distinctio minus quibusdam qui praesentium aliquam labore ex quam perferendis dolorum dicta non blanditiis inventore! Sapiente a perspiciatis, perferendis, ab quaerat porro dolore laudantium labore iusto eaque quod exercitationem, autem ullam eveniet.
+        <br/>
+        <About />
       </div>
+    </>
   );
 }
